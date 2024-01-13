45e7dc8096fee742753256c5082671f43df603cf
BorisMilanov <boris.mil.96@gmail.com>
2024-01-13 17:45:34 +0200

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,12 +4,12 @@ import bcrypt from "bcryptjs";
 export const register = async (req, res, next ) => {
     const role = await Role.find({role: 'User'});
     const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.passord, salt);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
         firstName: req.body.firstName,
         username: req.body.username,
         email: req.body.email,
-        passord: hashPassword,
+        password: hashPassword,
         roles: role
     });
     await newUser.save();
