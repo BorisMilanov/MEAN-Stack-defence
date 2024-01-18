541273aff10c985697a91b3e3071204c9421872a
BorisMilanov <boris.mil.96@gmail.com>
2024-01-18 13:47:39 +0200

    Register form setup 0.1

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder,FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export default class  RegisterComponent {
+export default class  RegisterComponent implements OnInit {
+  fb = inject(FormBuilder);
+  registerForm !: FormGroup;
+  ngOnInit(): void{
+    this.registerForm = this.fb.group({
+      firstName:['', Validators.required],
+      username:['', Validators.required],
+      email:['', Validators.compose([Validators.required, Validators.email])],
+      password:['', Validators.required],
+    })
+  }
 
+  register(){
+    console.log(this.registerForm.value);
+    
+  }
 }
